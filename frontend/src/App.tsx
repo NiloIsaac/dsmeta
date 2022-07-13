ac039d71535c84633babbd5c606a39b2e33a7a2d
Nilo Isaac <niloisaac2@gmail.com>
2022-07-13 14:19:54 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
 import NotificationButton from "./components/NotificationButton"
-
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 function App() {
-  return(
+  return (
     <>
-    <h1>Olá Theo!</h1>
-    <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+
+
+
+
+          </div>
+        </section>
+      </main>
     </>
 
   )
