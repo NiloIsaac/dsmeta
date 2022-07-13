04a7be7f802921c45f3b73f266a7dd3e7ee86c6c
Nilo Isaac <niloisaac2@gmail.com>
2022-07-13 09:28:35 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return(
-    <h1>Olá mundo!</h1>
+    <>
+    <h1>Olá Theo!</h1>
+    <NotificationButton />
+    </>
+
   )
 }
 
